26fd973f1d6ff88aa5f20691bbb2283499acd9e5
Alexander <alavrukhine@rixtrema.com>
2021-08-13 12:36:22 +0300

    add searchlinks to proxy

--- proxy16/peertube/index.js
+++ proxy16/peertube/index.js
@@ -235,14 +235,6 @@ var Peertube = function (settings) {
           });
 
           return d;
-
-          return data.flat().reduce(
-            (accumulator, currVal) => ({
-              ...accumulator,
-              [currVal.host]: currVal.data,
-            }),
-            {},
-          );
         })
         .catch((e = {}) =>
           Promise.reject({
